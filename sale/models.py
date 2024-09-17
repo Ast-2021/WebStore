5271bf0e5fd2ad67cd4f3900929e74802c1538e9
Stanislaw <desert7987@gmail.com>
2024-09-17 15:14:13 +0500

    Создание модели Пользователей

--- sale/models.py
+++ sale/models.py
@@ -1,26 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 
-class BaseModel(models.Model):
-    objects = models.Manager()
-    class Meta:
-        abstract = True
 
-
-class Product(BaseModel):
+class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(null=True)
     image = models.ImageField(upload_to='images/%Y/%m/%d/')
-    phone = models.IntegerField()
     category = models.ForeignKey('Category', on_delete=models.CASCADE, null=True)
+    author = models.ForeignKey('User', on_delete=models.CASCADE)
     date = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return self.title
 
 
-class Category(BaseModel):
+class Category(models.Model):
     title = models.CharField(max_length=255)
 
     def __str__(self):
         return self.title
+
+
+class User(models.Model):
+
+    REQUIRED_FIELDS = ('user',)
+    USERNAME_FIELD = 'email'
+    is_anonymous = ''
+    is_authenticated = ''
+
+    name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    photo = models.ImageField(upload_to='users/%Y/%m/%d', null=True, verbose_name='Фотография', default='users/standard_avatar.png')
+    password = models.CharField(max_length=55)
+
+
+    
